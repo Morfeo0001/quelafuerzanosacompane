76611e2abd37697e8e2be9d652e98018b04fd3a9
Morfeo0001 <thetankesilva1@gmail.com>
2026-05-13 19:45:39 -0300

    agregandocss

--- src/App.jsx
+++ src/App.jsx
@@ -12,3 +12,20 @@ function App() {
 
           <span>40.7128° N / 74.0060° W</span>
         </div>
+         <div className="main-title">
+          <h1>NEW YORK</h1>
+        </div>
+
+        <nav className="menu">
+          <a href="#">Inicio</a>
+          <a href="#">Historia</a>
+          <a href="#">Lugares</a>
+          <a href="#">Contacto</a>
+        </nav>
+
+      </header>
+    </>
+  )
+}
+
+export default App
